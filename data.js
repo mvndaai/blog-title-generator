2e347051f04906fce1061a686c2aeae4f7c202b4
Jason Mavandi <jmavandi@solutionreach.com>
2021-02-16 22:12:37 -0700

    add more options

--- data.js
+++ data.js
@@ -1,16 +1,29 @@
 var data = {
     'target': [
+        'country music',
         'science',
         'horror films',
         'italy',
+        'urban planning',
+        'motorsport',
+        'financial services',
+        'haristyling',
+        'beer',
+        'economics',
+        'medicine',
     ],
     'gripe': [
         'transfobia',
         'sexism',
-        'ageism'
+        'ageism',
+        'homophobia',
+        'Islamophobia',
+        'racism',
     ],
     'snazz': [
         'why are we ignoring the',
-        'yes we stilll have a',
+        'yes, we stilll have a',
+        'we need to talk about the',
+        `it's time to address the`,
     ]
 }
