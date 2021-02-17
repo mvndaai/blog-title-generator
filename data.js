6c6c1b6e4655c25798b23e2de3ace6f0053f2ecc
Jason Mavandi <jmavandi@solutionreach.com>
2021-02-16 21:30:30 -0700

    create working page

---
+++ data.js
@@ -0,0 +1,16 @@
+var data = {
+    'target': [
+        'science',
+        'horror films',
+        'italy',
+    ],
+    'gripe': [
+        'transfobia',
+        'sexism',
+        'ageism'
+    ],
+    'snazz': [
+        'why are we ignoring the',
+        'yes we stilll have a',
+    ]
+}
